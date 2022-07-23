53d869c27b1e299f4983ae6583ce80d2805fa005
Michał Bielecki <mb9246@gmail.com>
2022-07-23 15:27:14 +0200

    datePicker hourPicker actualized in Booking class

--- src/js/components/DatePicker.js
+++ src/js/components/DatePicker.js
@@ -15,20 +15,32 @@ class DatePicker extends BaseWidget{
 
     thisWidget.minDate = new Date();
     thisWidget.maxDate = utils.addDays(thisWidget.minDate, settings.datePicker.maxDaysInFuture);
+
+    /*  */
     // eslint-disable-next-line no-undef
-    flatpickr(thisWidget.dom.input, {
-      defaultDate: thisWidget.minDate,
-      minDate: thisWidget.minDate,
-      maxDate: thisWidget.maxDate,
+    flatpickr(thisWidget.dom.input, { 
+      // 1.arg = referencja do inputu, na którym plugin ma się uruchomić. 
+      // 2. arg = ustawienia
+
+      defaultDate: thisWidget.minDate,  //data domyślna = data obecna
+      minDate: thisWidget.minDate, //data minimalna = data obecna (rezerwacja w przeszłość?)
+      maxDate: thisWidget.maxDate, //data obecna + dni wybrane przez użytkownika
       locale: {
+
+        /* Ustala, aby pierwszym dniem tygodnia zawsze był poniedziałek */
         firstDayOfWeek: 1
       },
+
+      /* Nasza restauracja jest nieczynna w poniedziałki */
       disable: [
         function(date) {
           return (date.getDay() === 1);
         }
       ],
+      /* funkcja callback (onChange). Uruchamiana, gdy plugin wykryje zmianę terminu */
       onChange: function(selectedDates, dateStr) {
+
+        /* wynikiem jej działania będzie po prostu zaktualizowanie thisWidget.value */
         thisWidget.value = dateStr;
       },
     });
--- src/js/components/booking.js
+++ src/js/components/booking.js
@@ -1,5 +1,7 @@
 import {select, templates} from '../settings';
 import AmountWidget from './amountwidget';
+import HourPicker from './HourPicker';
+import DatePicker from './DatePicker';
 
 class Booking {
   constructor(element){
@@ -27,6 +29,10 @@ class Booking {
     thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
     thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
 
+    //2 nowe referencje do datePicker i hourPicker
+    thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
+
+    thisBooking.dom.hoursPicker = thisBooking.dom.wrapper.querySelector(select.widgets.hourPicker.wrapper);
   }
 
   initWidgets(){
@@ -39,7 +45,12 @@ class Booking {
     thisBooking.hoursAmountWidget = new AmountWidget(thisBooking.dom.hoursAmount);
     thisBooking.dom.hoursAmount.addEventListener('click', function(){});
 
+    /* uruchomienie na obu elementach odpowiednie widgety. (datePicker, hourPicker*/
+    thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
+    thisBooking.dom.hourPicker.addEventListener('input', function(){});
 
+    thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
+    thisBooking.dom.datePicker.addEventListener('click', function(){});
   }
 }
 
