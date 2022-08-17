a3485dd2a91a335f247e0454163673d1116a9566
Michał Bielecki <mb9246@gmail.com>
2022-08-17 10:52:51 +0200

    netlify

---
+++ server.js
@@ -0,0 +1,16 @@
+/* global require, process */
+/* Jest to kod, który nie będzie uruchamiany w przeglądarce, tylko na serwerze. */
+
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('dist/db/app.json');
+const middlewares = jsonServer.defaults({
+  static: 'dist',
+  noCors: true
+});
+const port = process.env.PORT || 3131;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
